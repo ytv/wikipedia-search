29d8850adb13e18545a3b79631580672dc868bad
ytv <sillying@gmail.com>
2016-06-01 11:47:22 -0700

    Added functionality of triggering search results when 3 or more characters are entered in the search box and nothing has been entered in the past 300 ms.  Code was also refactored.

--- public/js/app.js
+++ public/js/app.js
@@ -3,67 +3,97 @@ $(document).ready( function() {
 });
 
 var App = {
-  apiURL: {
-    endpoint: "https://en.wikipedia.org/w/api.php",
-    query: "",
+  api: {
+    endpoint: 'https://en.wikipedia.org/w/api.php',
+    query: '',
     parameters: {
       sentences: 15,
       limit: 10
     },
   },
   // The column values below are used to change change the Bootstrap grid layout
-  gridParams: {
-    colXsImg: 3,
-    colXsText: 9,
-    colSmImg: 2,
-    colSmText: 10,
-    colSmOffset: 2,
-    colMdImg: 2,
-    colMdText: 10
+  css: {
+      img: {
+          xs: 'col-xs-3',
+          sm: 'col-sm-2',
+          md: 'col-md-2'
+      },
+      text: {
+          xs: 'col-xs-9',
+          sm: 'col-sm-10',
+          md: 'col-md-10'
+      },
+      offset: {
+          sm: 'col-sm-offset-2'
+      }
   },
+
+  results: '.results',
+
+  triggerSubmit: null,
+
   init: function() {
-    $("#searchBox").keyup(function(event) {
+    $('#searchBox').on('focus', function() {
+      App._updateSearchBox();
+      App._clearRandomSearch();
+
+      $('#searchBox').on('keydown', function() {
+          clearTimeout(App.triggerSubmit);
+          if($('#searchBox').val().length >= 2)
+            App._triggerSubmit();
+      });
+    });
+
+    // triggers "Enter" key
+    $('#searchBox').keyup(function(event) {
       if(event.keyCode == 13) {
         App._submitQuery();
       }
     })
   },
+
+  _triggerSubmit: function() {
+      App.triggerSubmit = setTimeout(function() {
+        App._submitQuery();
+    }, 300);
+  },
+
   _submitQuery: function() {
-    var query = $("#searchBox").val();
-    $(".results").text("");
+    clearTimeout(App.triggerSubmit);
+    var query = $('#searchBox').val();
+    $(App.results).text('');
     App._searchWiki(query);
   },
 
   _searchWiki(query) {
     $.ajax({
-      url: App.apiURL.endpoint +
-        "?action=query&format=json&prop=extracts%7Cpageimages&generator=search&piprop=thumbnail&pithumbsize=200&pilimit=max&callback=?" +
-        "&exsentences=" + App.apiURL.parameters.sentences +
-        "&exlimit=" + App.apiURL.parameters.limit +
-        "&exintro=" + App.apiURL.parameters.limit +
-        "&gsrlimit=" + App.apiURL.parameters.limit +
-        "&indexpageids&gsrsearch=" +
+      url: App.api.endpoint +
+        '?action=query&format=json&prop=extracts%7Cpageimages&generator=search&piprop=thumbnail&pithumbsize=200&pilimit=max&callback=?' +
+        '&exsentences=' + App.api.parameters.sentences +
+        '&exlimit=' + App.api.parameters.limit +
+        '&exintro=' + App.api.parameters.limit +
+        '&gsrlimit=' + App.api.parameters.limit +
+        '&indexpageids&gsrsearch=' +
         query,
       dataType: 'json',
       type: 'GET',
       headers: { 'Api-User-Agent': 'Example/1.0' },
       success: function(data, textStatus, jqXHR) {
-        App._updateSearchBox();
-        App._clearRandomSearch();
+        // App._updateSearchBox();
         // App._clearRandomSearch();
 
-        var results = '<div class="row"><div class="col-sm-10 col-sm-offset-2"><u>Results for "' + query + '</u>":</div></div><br>';
+        var results = '<div class="row row-header"><div class="' + App.css.text.sm + ' ' + App.css.offset.sm + '"><u>Results for "' + query + '</u>":</div></div><br>';
         $(".resultsHeader").html(results);
         // Error handling for the search results.
         if(!data.error && data.query) {
           var pages = data.query.pages;
-          // Page relevance seems to correspond with the index property.  The
-          // pages object is made of several individual page objects, each of
-          // which have their own index property.  Therefore, an array
-          // "pageSortedByIndex" is created made of [pageId:index values] array
-          // elements, which are then sorted based on the index values.  The
-          // now sorted pageIds are then fed into the "App._showResults"
-          // function to display the results by relevance.
+        /* Page relevance seems to correspond with the index property.  The
+           pages object is made of several individual page objects, each of
+           which have their own index property.  Therefore, an array
+           'pageSortedByIndex' is created made of [pageId:index values] array
+           elements, which are then sorted based on the index values.  The
+           now sorted pageIds are then fed into the 'App._showResults'
+           function to display the results by relevance. */
           var pageSortedByIndex = [];
           for (var pageId in pages) {
             pageSortedByIndex.push([pageId, pages[pageId].index]);
@@ -74,17 +104,19 @@ var App = {
           App._showResults(pageSortedByIndex, pages, query);
         }
         else {
-          var htmlText = '<div class="row"><div class="result col-sm-' + App.gridParams.colSmText +
-            ' col-sm-offset-' + App.gridParams.colSmOffset + '">';
-          htmlText += "Sorry, your search turned up no results.</div></div>"
-          $(".results").append(htmlText);
+          var htmlText = '<div class="row">';
+          htmlText += '<div class="result ' + App.css.text.sm +' ' + App.css.offset.sm + '">';
+          htmlText += 'Sorry, your search turned up no results.';
+          htmlText += '</div>';
+          htmlText += '</div>';
+          $(App.results).append(htmlText);
         }
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log("AJAX request error: " + JSON.stringify(jqXHR, null, 2));
         // alert("AJAX request error: " + jqXHR.statusText);
           var htmlText = '<font color="red">AJAX request Error</font>';
-          $(".error").append(htmlText);
+          $('.error').append(htmlText);
       }
     })
   },
@@ -97,39 +129,38 @@ var App = {
       var extract = pages[pageId].extract;
 
       // Below filters out irrelevant results, such as the ones that only
-      // refer you to other pages ("may refer to").
+      // refer you to other pages ('may refer to').
       if(extract.length < title.length + 30)
         continue;
 
       // HTML setup for the thumbnail images
-      var text = '<div class="row"><div class="imgResults col-md-' + this.gridParams.colMdImg +
-        ' col-sm-' + this.gridParams.colSmImg +
-        ' col-xs-' + this.gridParams.colXsImg + '">';
+      var text = '<div class="row">';
+      text += '<div class="imgResults ' + App.css.img.md + ' ' + App.css.img.sm + ' ' + App.css.img.xs + '">';
       // Below checks for whether a thumbnail image is available
       if(pages[pageId].thumbnail != undefined) {
         var imgSrc = "";
         imgSrc = pages[pageId].thumbnail.source;
         text += '<img class="img-responsive" src="' + imgSrc + '"/>'
       }
-      text += '</div>'; // Div for grid
+      text += '</div>';     // div for grid
 
-      // HTML setup for the "text" part of the search results
-      text += '<div class="result col-md-' + this.gridParams.colMdText +
-        ' col-sm-' + this.gridParams.colSmText +
-        ' col-xs-' + this.gridParams.colXsText + '">';
-      text += '<div class="resultTitle"><b><a class="resultLink" target="_blank" href="' + link + '">' + title + '</b></a></div>';
+      // HTML setup for the 'text' part of the search results
+      text += '<div class="result ' + App.css.text.md + ' ' + App.css.text.sm + ' ' + App.css.text.xs + '">';
+      text += '<div class="resultTitle">';
+      text += '<a class="resultLink" target="_blank" href="' + link + '"><b>' + title + '</b></a>';
+      text += '</div>';     // div for title
       text += extract;
-      text += '</div></div><br><br>'; // Div for grid & row
-      $(".results").append(text);
+      text += '</div>';     //div for grid
+      text += '</div>';     //div for row
+      $(App.results).append(text);
     }
 
   },
   _updateSearchBox: function() {
-    $("#searchBox").val("");
-    document.getElementById("searchBox").placeholder = "";
-    $(".box").css({"margin":"2% 0", "float":"right"});
+    document.getElementById('searchBox').placeholder = '';
+    $('.box').addClass('moveToTopRight');
   },
   _clearRandomSearch: function() {
-    $(".container-2").html("");
+    $('.container-2').html('');
   }
 }
